cf216c9de6b8f44bc9b67237395b67951d062b1e
gw1n3ks <124576196+gw1n3ks@users.noreply.github.com>
2023-07-09 05:03:13 +0400

    savr

--- swiper-slider.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: "auto",
-  spaceBetween: 30,
-  grabCursor: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
